70ef89bc0fc9801ccbf020aa6a0e58488e7a72c2
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-11-01 14:00:22 +0000

    inital commit

---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div v-for="letter in letters">
+      <img :src="letter.url" :alt="letter.letter">
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      letters: []
+    }
+  },
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ server/db/seeds.js
@@ -0,0 +1,109 @@
+use sign_language;
+db.dropDatabase();
+
+db.letters.insertMany([
+ {
+   letter: "a",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-508-1.jpg"
+ },
+ {
+   letter: "b",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-509-1.jpg"
+ },
+ {
+   letter: "c",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-510-1.jpg"
+ },
+ {
+   letter: "d",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-511-1.jpg"
+ },
+ {
+   letter: "e",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-512-1.jpg"
+ },
+ {
+   letter: "f",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-513-1.jpg"
+ },
+ {
+   letter: "g",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-514-1.jpg"
+ },
+ {
+   letter: "h",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-515-1.jpg"
+ },
+ {
+   letter: "i",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-516-1.jpg"
+ },
+ {
+   letter: "j",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-517-1.jpg"
+ },
+ {
+   letter: "k",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-518-1.jpg"
+ },
+ {
+   letter: "l",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-519-1.jpg"
+ },
+ {
+   letter: "m",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-520-1.jpg"
+ },
+ {
+   letter: "n",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-521-1.jpg"
+ },
+ {
+   letter: "o",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-522-1.jpg"
+ },
+ {
+   letter: "p",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-523-1.jpg"
+ },
+ {
+   letter: "q",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-524-1.jpg"
+ },
+ {
+   letter: "r",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-525-1.jpg"
+ },
+ {
+   letter: "s",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-526-1.jpg"
+ },
+ {
+   letter: "t",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-527-1.jpg"
+ },
+ {
+   letter: "u",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-528-1.jpg"
+ },
+ {
+   letter: "v",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-529-1.jpg"
+ },
+ {
+   letter: "w",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-530-1.jpg"
+ },
+ {
+   letter: "x",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-531-1.jpg"
+ },
+ {
+   letter: "y",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-532-1.jpg"
+ },
+ {
+   letter: "z",
+   url: "https://media.spreadthesign.com/image/500/alphabet-letter-533-1.jpg"
+ }
+])
