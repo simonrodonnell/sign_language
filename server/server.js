75107a849d22114302f01b6c2aad56c637ed8b34
Iona Grant <ionagrant92@gmail.com>
2019-11-01 14:44:05 +0000

    server js finished for letters

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('sign_language');
+  const lettersCollection = db.collection('letters');
+  const lettersRouter = createRouter(lettersCollection);
+  app.use('/api/letters', lettersRouter);
+})
+.catch(console.err);
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${this.address().port}`);
+});
