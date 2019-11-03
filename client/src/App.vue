3e4a22f56069eae7f3bed4bb33713a1fb71ee5ba
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-11-03 10:58:39 +0000

    added text to sign input

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,66 @@
 <template>
   <div id="app">
-    <div v-for="letter in letters">
-      <img :src="letter.url" :alt="letter.letter">
+    <input v-on:keyup="searchForLetter" type="text" v-model="textToSignLanguage">
+    <button @click="convertText">Convert String to Sign Language</button>
+    <div v-if="imageURL" id="imageDisplay">
+      <img :src="imageURL" alt="">
     </div>
+    <!-- <div v-for="letter in letters">
+    <img :src="letter.url" :alt="letter.letter">
+  </div> -->
+  <div v-for="phrase in phrases">
+    <p>{{phrase.phrase}}</p>
+    <video :src="phrase.videoUrl" controls></video>
+    <br>
   </div>
+</div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import SignLanguageService from './services/sign_language_service';
 
 export default {
   name: 'app',
   data () {
     return {
-      letters: []
+      letters: [],
+      phrases: [],
+      textToSignLanguage: "",
+      imageURL: ""
+    }
+  },
+  mounted(){
+    this.fetchData();
+  },
+  methods: {
+    fetchData(){
+      SignLanguageService.getLetters()
+      .then(letters => this.letters = letters);
+      SignLanguageService.getPhrases()
+      .then(phrases => this.phrases = phrases);
+    },
+    searchForLetter() {
+      let array = this.textToSignLanguage.split("");
+      let index = array.length - 1
+      let search = array[index]
+      if (search === " ") {
+        return
+      }
+      else
+      {
+        let result = this.letters.find((letter) => {
+          return letter.letter.toLowerCase() === search.toLowerCase()
+        })
+        this.imageURL = result.url
+      }
+    },
+    convertText() {
+      let letterArray = this.textToSignLanguage.split("");
+      // console.log(letterArray);
+      return letterArray.every((Arrayletter) => {
+
+      })
     }
   },
   components: {
