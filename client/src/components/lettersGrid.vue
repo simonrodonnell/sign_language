3a1ca52675346e9ad2109481e4fdfd86aeb9f6c2
Iain Tooley <iaintooley1@gmail.com>
2019-11-04 09:43:45 +0000

    created component letters grid

---
+++ client/src/components/lettersGrid.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+<div class="">
+  <h2>Letters in Sign Language</h2>
+  
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
