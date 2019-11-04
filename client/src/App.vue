6cbcbbc578d4dbd0b3a253f59f1cf8399281c8d5
Iain Tooley <iaintooley1@gmail.com>
2019-11-04 10:31:38 +0000

    fixed did you know

--- client/src/App.vue
+++ client/src/App.vue
@@ -49,8 +49,8 @@ export default {
   mounted(){
     this.fetchData();
     setInterval(() => {
-      this.randomFact = this.facts[Math.floor(Math.random()*facts.length)]
-    }, 3000);
+      this.randomFact = this.facts[Math.floor(Math.random()*this.facts.length)]
+    }, 10000);
   },
   methods: {
     fetchData(){
