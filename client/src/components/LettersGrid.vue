d2dc7c21a5cd1038e938eb6b7e3db2a45bdced9b
Iain Tooley <iaintooley1@gmail.com>
2019-11-04 14:07:25 +0000

    fuck u git

--- client/src/components/LettersGrid.vue
+++ client/src/components/LettersGrid.vue
@@ -1,22 +1,45 @@
 <template lang="html">
 <div class="">
   <h2>Letters in Sign Language</h2>
-  <!-- <input v-on:keyup="searchForLetter" type="text" v-model="textToSignLanguage">
-  <button @click="convertText">Convert String to Sign Language</button>
+  <input v-on:keyup="searchForLetter" type="text" v-model="textToSignLanguage">
+  <!-- <button @click="convertText">Convert String to Sign Language</button> -->
   <div v-if="imageURL" id="imageDisplay">
-  <img :src="imageURL" alt="">
-</div> -->
-  <select class="" name="">
+    <img :src="imageURL" alt="">
+  </div>
+  <!-- <select class="" name="">
     <option :value="anything" disabled></option>
     <option :value="sign.value" v-for="(sign, index) in letters" :key="index" :sign="sign">{{sign.letter}}</option>
-  </select>
+  </select> -->
+
 </div>
 </template>
 
 <script>
 export default {
   name: "letters-grid",
-  props: ["letters"]
+  props: ["letters"],
+  data(){
+    return {
+      textToSignLanguage: "",
+      imageURL: ""
+    }
+  },
+  methods: {
+    searchForLetter() {
+    let lastIndex = this.textToSignLanguage.length - 1
+    let search = this.textToSignLanguage[lastIndex]
+    if (search === " ") {
+      return
+    }
+    else
+    {
+      let result = this.letters.find((letter) => {
+        return letter.letter.toLowerCase() === search.toLowerCase()
+      })
+      this.imageURL = result.url
+    }
+  }
+}
 }
 </script>
 
