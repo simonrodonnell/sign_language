3e0bbe06ae8d65b83d2a727a0ef97f634cc8c4dc
Iain Tooley <iaintooley1@gmail.com>
2019-11-04 11:04:39 +0000

    will it work who knows

---
+++ client/src/components/LettersGrid.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+<div class="">
+  <h2>Letters in Sign Language</h2>
+  <select class="" name="">
+    <option :value="anything" disabled></option>
+    <option :value="sign.value" v-for="(sign, index) in letters" :key="index" :sign="sign">{{sign.letter}}</option>
+  </select>
+</div>
+</template>
+
+<script>
+export default {
+  name: "letters-grid",
+  props: ["letters"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
