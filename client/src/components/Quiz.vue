acb093bc91c1f5310f3a1e757fe8b13e12405295
Iain Tooley <iaintooley1@gmail.com>
2019-11-05 11:44:28 +0000

    added commponent setup for quiz

---
+++ client/src/components/Quiz.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="">
+
+    <button type="submit" name="submit">Submit Answers</button>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
